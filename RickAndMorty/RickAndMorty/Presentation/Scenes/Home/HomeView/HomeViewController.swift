eef8a84793ac42b22de54657d500c5138422b419
Khaled <khaledrafat04@yahoo.com>
2024-12-08 01:31:38 +0200

    Removing xib Files

--- RickAndMorty/RickAndMorty/Presentation/Scenes/Home/HomeView/HomeViewController.swift
+++ RickAndMorty/RickAndMorty/Presentation/Scenes/Home/HomeView/HomeViewController.swift
@@ -14,7 +14,8 @@ class HomeViewController: UIViewController {
     var baseVCActions: BaseViewAction?
     
     //Outlets
-    @IBOutlet weak var collectionView: UICollectionView!
+    private var stackView: UIStackView!
+    private var collectionView: UICollectionView!
     
     //MARK: - ViewDidLoad
     override func viewDidLoad() {
@@ -29,8 +30,9 @@ class HomeViewController: UIViewController {
             baseVCActions = DefaultBaseViewAction()
         }
         self.title = "Characters"
-        collectionSetup()
+        self.view.backgroundColor = .white
         setupStackView()
+        createCollection()
         viewModel?.viewDidLoad()
     }
     
@@ -65,11 +67,92 @@ class HomeViewController: UIViewController {
             }
         })
     }
+}
+
+extension HomeViewController {
+    //MARK: - Setup Stack
+    private func setupStackView() {
+        self.stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        
+        let button1 = createButton(title: "All", tag: 1)
+        let button2 = createButton(title: "Alive", tag: 2)
+        let button3 = createButton(title: "Dead", tag: 3)
+        let button4 = createButton(title: "Unknown", tag: 4)
+        
+        stackView.addArrangedSubview(button1)
+        stackView.addArrangedSubview(button2)
+        stackView.addArrangedSubview(button3)
+        stackView.addArrangedSubview(button4)
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 10),
+            stackView.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
     
-    //MARK: - Collection
-    private func collectionSetup() {
-        collectionView.dataSource = self
+    //MARK: - Create Button
+    private func createButton(title: String, tag: Int) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.gray.cgColor
+        button.layer.cornerRadius = 20
+        button.tag = tag
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        return button
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            self.viewModel?.filter(with: .all)
+        case 2:
+            self.viewModel?.filter(with: .alive)
+        case 3:
+            self.viewModel?.filter(with: .dead)
+        case 4:
+            self.viewModel?.filter(with: .unknown)
+        default:
+            return
+        }
+    }
+    
+    //MARK: - Create Collection
+    func createCollection() {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(
+            width: self.view.frame.size.width,
+            height: 100
+        )
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        
         collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 10),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
